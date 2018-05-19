0d3a98fe533affcc6612503bf744746c5d6498e7
alicavdar <ac.alicavdar@gmail.com>
2018-05-20 02:27:10 +0300

    Remove dummy text

--- src/components/Evidence/Description/index.js
+++ src/components/Evidence/Description/index.js
@@ -8,12 +8,7 @@ const Description = ({ conclusion, text }) => (
   <Container>
     <ConclusionBadge conclusion={conclusion} />
     <P>
-      <Linkify>http://google.com Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Linkify>
+      <Linkify>{text}</Linkify>
     </P>
   </Container>
 )
