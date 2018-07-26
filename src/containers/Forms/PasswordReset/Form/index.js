0fcc208afccaa45ef038838a3fd57bed48c02e6f
alicavdar <ac.alicavdar@gmail.com>
2018-07-26 18:25:06 +0300

    Align password reset form

--- src/containers/Forms/PasswordReset/Form/index.js
+++ src/containers/Forms/PasswordReset/Form/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { reduxForm, Field } from 'redux-form'
 import { TextField } from 'components/Form'
+import { Flex, Box } from 'grid-styled'
 import { FORGOT_PASSWORD_FORM } from 'modules/user/constants'
 import { required, email } from 'core/validationRules'
 import Button from 'components/Button'
@@ -10,20 +11,25 @@ const PasswordResetForm = ({
   submitting,
 }) => (
   <form onSubmit={handleSubmit}>
-    <Field
-      type="text"
-      id="password_reset_email"
-      name="email"
-      label="Email"
-      autoComplete="email"
-      validate={[ required, email ]}
-      component={TextField} />
-
-    <Button
-      type='submit'
-      disabled={submitting}>
-      {submitting ? 'Submitting...' : 'Submit'}
-    </Button>
+    <Flex flexDirection="column">
+      <Box>
+        <Field
+          type="text"
+          id="password_reset_email"
+          name="email"
+          label="Email"
+          autoComplete="email"
+          validate={[ required, email ]}
+          component={TextField} />
+      </Box>
+      <Box alignSelf="flex-end" mt={10}>
+        <Button
+          type='submit'
+          disabled={submitting}>
+          {submitting ? 'Submitting...' : 'Submit'}
+        </Button>
+      </Box>
+    </Flex>
   </form>
 )
 
